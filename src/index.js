f4bf6b2501442518180ed84bdf40d2be8f97663c
David Provest <dpro7026@uni.sydney.edu.au>
2017-05-14 20:35:05 +1000

    Created first react class component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyC0Lw5aWwtDXnFxMEzB04QYy0kij5rdEPg'
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//Create a new component. This component should produce
+//some HTML
+// const App = function(){
+//   return <div>Hi!</div>;
+// }
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+//Take this component's generated HTML and put it
+//on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
